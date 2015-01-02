3021da0f9e35ed86ce453f03f7b39186eeae89d8
Matt Carter <m@ttcarter.com>
2015-01-02 14:56:12 +1000

    Added blocked parallel chain test

--- test/chains.js
+++ test/chains.js
@@ -93,3 +93,90 @@ describe('async-chainable - mixed chain', function(){
 		expect(outputSeries[6]).to.contain('13-then');
 	});
 });
+
+
+describe('async-chainable - blocked parallel() chain', function(){
+	var context;
+	var output, outputSections;
+
+	beforeEach(function(done) {
+		output = [];
+		outputSections = [];
+		context = {};
+
+		asyncChainable
+			.then(function(next) { outputSections.push('sec-1'); next() })
+			.parallel({
+				step1: function(next) { setTimeout(function(){ output.push('step1'); next(null, 'step1Value'); }, 0)},
+				step2: function(next) { setTimeout(function(){ output.push('step2'); next(null, 'step2Value'); }, 10)},
+				step3: function(next) { setTimeout(function(){ output.push('step3'); next(null, 'step3Value'); }, 5)},
+			})
+			.then(function(next) { outputSections.push('sec-2'); next() })
+			.parallel({
+				step4: function(next) { setTimeout(function(){ output.push('step4'); next(null, 'step4Value'); }, 0)},
+				step5: function(next) { setTimeout(function(){ output.push('step5'); next(null, 'step5Value'); }, 10)},
+				step6: function(next) { setTimeout(function(){ output.push('step6'); next(null, 'step6Value'); }, 10)},
+			})
+			// As an error was raised nothing until end() should now run
+			.then(function(next) { outputSections.push('sec-3'); next() })
+			.parallel({
+				step7: function(next) { setTimeout(function(){ output.push('step7'); next(null, 'step7Value'); }, 0)},
+				step8: function(next) { setTimeout(function(){ output.push('step8'); next(null, 'step8Value'); }, 10)},
+				step9: function(next) { setTimeout(function(){ output.push('step9'); next(null, 'step9Value'); }, 5)},
+			})
+			.end(function(err) {
+				expect(err).to.be.undefined();
+				outputSections.push('sec-end');
+				context = this;
+				done();
+			});
+	});
+
+	it('run all functions', function() {
+		expect(output).to.contain('step1');
+		expect(output).to.contain('step2');
+		expect(output).to.contain('step3');
+		expect(output).to.contain('step4');
+		expect(output).to.contain('step5');
+		expect(output).to.contain('step6');
+		expect(output).to.contain('step7');
+		expect(output).to.contain('step8');
+		expect(output).to.contain('step9');
+	});
+
+	it('should run all sections in the right order', function() {
+		expect(outputSections[0]).to.equal('sec-1');
+		expect(outputSections[1]).to.equal('sec-2');
+		expect(outputSections[2]).to.equal('sec-3');
+		expect(outputSections[3]).to.equal('sec-end');
+	});
+
+	it('should set the context', function() {
+		expect(context).to.have.property('step1');
+		expect(context.step1).to.equal('step1Value');
+
+		expect(context).to.have.property('step2');
+		expect(context.step2).to.equal('step2Value');
+
+		expect(context).to.have.property('step3');
+		expect(context.step3).to.equal('step3Value');
+
+		expect(context).to.have.property('step4');
+		expect(context.step4).to.equal('step4Value');
+
+		expect(context).to.have.property('step5');
+		expect(context.step5).to.equal('step5Value');
+
+		expect(context).to.have.property('step6');
+		expect(context.step6).to.equal('step6Value');
+
+		expect(context).to.have.property('step7');
+		expect(context.step7).to.equal('step7Value');
+
+		expect(context).to.have.property('step8');
+		expect(context.step8).to.equal('step8Value');
+
+		expect(context).to.have.property('step9');
+		expect(context.step9).to.equal('step9Value');
+	});
+});
