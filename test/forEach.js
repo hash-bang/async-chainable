bff8b6a00cc8a6c2c7b870244a8bbcf77b953559
Matt Carter <m@ttcarter.com>
2015-07-16 12:21:41 +1000

    Updated async to latest version Added large array iterator tests - currently failing

--- test/forEach.js
+++ test/forEach.js
@@ -379,3 +379,57 @@ describe('async-chainable.forEach() - named set', function(){
 		expect(outputCollection).to.contain({three: 'Number 3'});
 	});
 });
+
+
+describe('async-chainable.forEach() - large arrays (run time bound)', function(){
+	var output;
+	var limit = 99999;
+
+	before(function(done) {
+		output = [];
+
+		var data = [];
+		for (var i = 0; i < limit; i++) {
+			data.push('Item ' + i);
+		}
+
+		asyncChainable()
+			.forEach(data, function(next, item) { output.push(item); next(); })
+			.end(function(err) {
+				expect(err).to.be.not.ok;
+				done();
+			});
+	});
+
+	it('should have the correct number of output elements', function() {
+		expect(output).to.have.length(limit);
+	});
+});
+
+
+describe('async-chainable.forEach() - large arrays (late bound)', function(){
+	var output;
+	var limit = 99999;
+
+	before(function(done) {
+		output = [];
+
+		asyncChainable()
+			.then('data', function(next) {
+				var data = [];
+				for (var i = 0; i < limit; i++) {
+					data.push('Item ' + i);
+				}
+				next(null, data);
+			})
+			.forEach('data', function(next, item) { output.push(item); next(); })
+			.end(function(err) {
+				expect(err).to.be.not.ok;
+				done();
+			});
+	});
+
+	it('should have the correct number of output elements', function() {
+		expect(output).to.have.length(limit);
+	});
+});
